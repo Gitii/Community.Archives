4266c404996c7b798b7e188cca0f780c751ea568
Gitii <Gitii@users.noreply.github.com>
2022-01-20 23:18:12 +0100

    Add ci workflow (#1)
    * Add ci workflow
    * Set thresholds & fix workflow.

--- generate-coverage-report.cmd
+++ generate-coverage-report.cmd
@@ -1,4 +1,4 @@
-dotnet test /p:TargetFramework=net6.0 /p:CollectCoverage=true /p:Exclude=\"[Community.Archives.Core.Tests]*\" /p:CoverletOutput=../CoverageResults/ /p:MergeWith="../CoverageResults/coverage.json" /p:CoverletOutputFormat=\"cobertura,json\" /p:ThresholdType=\"line,branch,method\" -m:1
+generate-coverage.cmd
 
 rmdir /S /Q CoverageReport
 reportgenerator "-reports:.\CoverageResults\coverage.net6.0.cobertura.xml" "-targetdir:CoverageReport" "-reporttypes:Html;HtmlSummary"
